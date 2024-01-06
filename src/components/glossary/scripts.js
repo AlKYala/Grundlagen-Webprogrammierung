0910240b2d18314b879beed69e26ba327cac9234
Ali Yalama <aliyalama@googlemail.com>
2024-01-06 01:54:38 +0100

    Make glossary a page, fix filtering

--- src/components/glossary/scripts.js
+++ src/components/glossary/scripts.js
@@ -1,11 +1,12 @@
 function filterTerms() {
-    var input, filter, glossaryTerms, terms, i, termText;
-    input = document.getElementById('searchInput');
-    filter = input.value.toUpperCase();
-    glossaryTerms = document.getElementById('glossaryTerms');
-    terms = glossaryTerms.getElementsByClassName('term');
+    var input, filter, glossaryContainer, terms, termText;
+    input = document.getElementById('searchInput');  // Get the search input
+    filter = input.value.toUpperCase();  // Get the search term in uppercase
+    glossaryContainer = document.querySelector('.glossary-container');  // Get the glossary container
+    terms = glossaryContainer.getElementsByClassName('term');  // Get all terms within the container
 
-    for (i = 0; i < terms.length; i++) {
+    // Loop through all list items, and hide those that don't match the search query
+    for (var i = 0; i < terms.length; i++) {
         termText = terms[i].textContent || terms[i].innerText;
         if (termText.toUpperCase().indexOf(filter) > -1) {
             terms[i].style.display = "";

7f82f99ab5b1a814bf1c4323ff8fc7a70061a0f9
Ali Yalama <aliyalama@googlemail.com>
2024-01-14 21:21:12 +0100

    Placeholder and script for date last changed

--- src/scripts.js
+++ src/scripts.js
@@ -2,3 +2,12 @@ function parentRedirect(e, url) {
     e.preventDefault();  // Prevent the default anchor behavior
     top.location.href = url;  // Change the location of the topmost parent window
 }
+
+// Update the last modified date
+function updateLastModifiedDate() {
+    var lastModified = document.lastModified;
+    document.getElementById("lastModified").innerText = "Last Modified: " + lastModified;
+}
+
+// Call the function when the page loads
+window.onload = updateLastModifiedDate;

7fcc15af6402318f5a2aa0f82720c813fcc40ef8
Ali Yalama <aliyalama@googlemail.com>
2024-01-18 22:32:32 +0100

    Remove comments

--- src/scripts/firstRowNavbar.js
+++ src/scripts/firstRowNavbar.js
@@ -13,8 +13,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 function parentRedirect(e, url) {
-    e.preventDefault();  // Prevent the default anchor behavior
-    top.location.href = url;  // Change the location of the topmost parent window
+    e.preventDefault();
+    top.location.href = url;
 }
 
 function redirectToGlossary() {
